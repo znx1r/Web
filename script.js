686f46fb42a0a94eec85ac9a6c0804926c1a31e9
JuanRomero2005 <argentinajuanromero@gmail.com>
2025-10-20 12:05:19 +0200

    Reupload after username change

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const THUMBNAILS = [
   { image:"https://sp.deevid.ai/storage/v1/object/public/user-image/38460562-6830-4c2f-8abe-96fa1da9c706.png", title:"" },
   { image:"https://sp.deevid.ai/storage/v1/object/public/user-image/b247bc89-9d32-472b-9b4b-1fa651b82ef0.png", title:"" },
   { image:"https://sp.deevid.ai/storage/v1/object/public/user-image/10cdf614-8524-435d-a4a8-fc9fb9d7915d.png", title:"" },
-  { image:"file:///C:/Users/Juan/Downloads/white%20(3)%20(5)%20(1).jpg", title:"" },
+  { image:"https://cdn.discordapp.com/attachments/1158681587819479041/1429771866171773058/white_3_5_1.jpg?ex=68f75a50&is=68f608d0&hm=779abfc6be4ecc4a7d09ea8be62f96948ddde8eb4a1e057c8283281eebc4cb9b&", title:"" },
   { image:"https://sp.deevid.ai/storage/v1/object/public/user-image/3b8df1ce-9cf3-466a-8800-2324acd14d92.png", title:"" },
   { image:"https://sp.deevid.ai/storage/v1/object/public/user-image/4d45b624-0438-4fb4-9433-86cce4296359.png", title:"" },
   { image:"https://cdn.discordapp.com/attachments/1417274577523834911/1429416342087536670/image.png?ex=68f6b7f5&is=68f56675&hm=bf031202eb4b10686db218eaa828f3e042a8bb6d4958cf4c9f8e1a67c3b86947", title:"" },
